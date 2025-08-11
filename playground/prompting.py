c54ec53e57ae38409943bb6ea29b1f460b0a6b9b
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-08-11 06:54:12 -0700

    Beginning of prompting through templates
    Not used yet

---
+++ playground/prompting.py
@@ -0,0 +1,9 @@
+from assist.promptable import Promptable
+
+class MyClass(Promptable):
+    def my_prompt_file(self):
+        return self.prompt_file()
+
+mc = MyClass()
+mc.prompt_for("test_template.txt",
+              here="Here it is")
