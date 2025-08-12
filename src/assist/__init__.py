b785cf0f112f70fd96aebef5a0811f66befb8e88
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-08-11 20:38:11 -0700

    Integrate planner into reflexion graph and streamline prompts

--- src/assist/__init__.py
+++ src/assist/__init__.py
@@ -1,7 +1,3 @@
-from .reflexion_agent import (
-    reflexion_agent,
-    PlannerAgent,
-    ReflexionAgent,
-)
+from .reflexion_agent import reflexion_agent
 
-__all__ = ["reflexion_agent", "PlannerAgent", "ReflexionAgent"]
+__all__ = ["reflexion_agent"]
