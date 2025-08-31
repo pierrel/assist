d680ec43edc18685d941eda6691723945c0480a8
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-08-30 18:31:15 -0700

    Adding some eval examples

---
+++ playground/eval/plan_eval.py
@@ -0,0 +1,48 @@
+from typing import Optional, Callable
+from pydantic import BaseModel
+from assist.reflexion_agent import build_plan_node, ReflexionState
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from assist.tools.base import base_tools
+from langchain.callbacks.tracers.stdout import ConsoleCallbackHandler
+from langgraph.graph import StateGraph, END
+from langgraph.graph.state import CompiledStateGraph
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
+class EvalRecord(BaseModel):
+    ok: bool
+    wall_ms: float
+    tool_calls: int
+    prompt_tokens: Optional[int]
+    completion_tokens: Optional[int]
+    input: str
+    output: str
+
+def graphiphy(node: Callable,
+              state: BaseModel) -> CompiledStateGraph:
+    graph = StateGraph(state)
+
+    graph.add_node("node", node)
+    graph.set_entry_point("node")
+    graph.add_edge("node", END)
+    return graph.compile()
+
+
+def plan_eval_simple():
+    llm = ChatOpenAI(model="gpt-4o-mini",
+                     temperature=0.8)
+    node = build_plan_node(llm,
+                           base_tools("~/.cache/assist/dbs"),
+                           [ConsoleCallbackHandler()])
+    graph = graphiphy(node, ReflexionState)
+    state = ReflexionState(messages=[SystemMessage("You are a helpful assistant"),
+                                     HumanMessage("What's the 200th digit of pi?")],
+                           plan=[],
+                           step_index=0,
+                           history=[],
+                           needs_replan=False,
+                           learnings=[])
+    return graph.invoke(state)
+
+plan_eval_simple()
