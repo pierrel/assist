68473df42f6200ef26a0ef76ae4435684874f937
Pierre Larochelle <pierre@quirozlarochelle.com>
2025-09-07 15:25:06 -0700

    Adding safe python playground script

---
+++ playground/play_safe_python.py
@@ -0,0 +1,12 @@
+from assist.general_agent import general_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from assist.tools.safe_python import SafePythonTool
+
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.4)
+agent = general_agent(llm, [SafePythonTool()])
+
+resp = agent.invoke({"messages": [HumanMessage(content="If I save $100 every month in an account that started with $10,000 and provides an interest rate of 3%, how much will the account have in 10 years?")]})
+print(resp)
+                                  
