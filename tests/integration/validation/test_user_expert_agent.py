47e7b2945370ed4a1f8956b91964c960afbb1e4b
Pierre Larochelle <pierre@quirozlarochelle.com>
2026-01-23 05:40:33 -0800

    Moving test file functions to utils

--- tests/integration/validation/test_user_expert_agent.py
+++ tests/integration/validation/test_user_expert_agent.py
@@ -11,34 +11,7 @@ from langgraph.graph.state import CompiledStateGraph
 from langchain_core.messages import HumanMessage, AIMessage, ToolMessage, BaseMessage
 from assist.model_manager import select_chat_model
 
-def read_file(path: str):
-    """Returns the full contents of file at path"""
-    with open(path, 'r') as f:
-        return f.read()
-
-def create_filesystem(root_dir: str,
-                      structure: dict):
-    """Creates a directory structure and files according to `structure`. For example:
-    {"README.org": "This is the readme file",
-    "gtd": {"inbox.org": "This is the inbox file"},
-           {"projects": {"project1.org": "This is a project file"}}}
-
-    Creates:
-    a README.org file with content "This is the readme file"
-    a gtd directory
-    a gtd/inbox.org file with content "This is the inbox file"
-    ..."""
-    for name, content in structure.items():
-        path = os.path.join(root_dir, name)
-
-        if isinstance(content, str):
-            # Create a file with the given content
-            with open(path, 'w') as f:
-                f.write(content)
-        elif isinstance(content, dict):
-            # Create a directory and recursively process its contents
-            os.makedirs(path, exist_ok=True)
-            create_filesystem(path, content)
+from .utils import read_file, create_filesystem
 
 class TestUserExpertAgent(TestCase):
     def create_agent(self, filesystem: dict):
