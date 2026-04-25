e6b3567115ac341e585c60323c88b849a95475b4
Pierre Larochelle <pierre@quirozlarochelle.com>
2026-04-25 07:51:14 -0700

    Adding find_dotenv

--- edd/conftest.py
+++ edd/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 
 @pytest.fixture(scope="session", autouse=True)
 def configure_env():
     print("Loading dev dotenv")
-    if load_dotenv(".dev.env"):
+    if load_dotenv(find_dotenv(".dev.env")):
         print("Loaded dev dotenv")
     else:
         print("Could not find dev dotenv")
